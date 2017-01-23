97ec3009d04149d2a617e42d34d4f3755f354d32
Stefano <zwonimir@live.it>
2017-01-23 22:24:56 +0400

    Configured bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "/src/jquery.observer.js",
+  entry: "./src/jquery.observer.js",
   output: {
-    filename: "/dist/bundle.js"
-  }
+    filename: "./dist/bundle.js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ], 
+  watch: true
 }
